59880501706133149c3e354afaf2f44e906f718a
obsidian12 <erhnknum18@gmail.com>
2024-12-21 16:16:35 +0900

    remove old files

--- run_train/main.py
+++ run_train/main.py
@@ -1,28 +1,4 @@
-# ********************************************************************************************
-# 파일명 : main.py
-# 목적　 : 특정 DNN을 학습시킬 때 최종적으로 실행되는 파일
-# 주의 　: CNNTrain_***.py 파일을 import 하여 그 안에 포함된 학습 함수를 사용할 것! 
-# ********************************************************************************************
+from trainer import *
 
-import CNNTrain_CIFAR as cf
-
-cf.training_cifar(6363)
-cf.training_cifar(8052)
-cf.training_cifar(5299)
-cf.training_cifar(2170)
-cf.training_cifar(7718)
-cf.training_cifar(6365)
-cf.training_cifar(4534)
-cf.training_cifar(1756)
-cf.training_cifar(542)
-cf.training_cifar(6032)
-cf.training_cifar(7163)
-cf.training_cifar(7182)
-cf.training_cifar(4204)
-cf.training_cifar(3638)
-cf.training_cifar(6946)
-cf.training_cifar(2892)
-cf.training_cifar(1542)
-cf.training_cifar(8704)
-cf.training_cifar(8465)
-cf.training_cifar(9348)
+tr = CIFAR10Trainer()
+tr.training("vgg16", 239, "./config.json")
